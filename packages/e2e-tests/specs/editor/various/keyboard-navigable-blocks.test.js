2af7ddb1e622982025bb801eaf560849e81aab1f
Ella van Durpe <ella@vandurpe.com>
2019-12-18 11:31:10 +0100

    Fix e2e tests

--- packages/e2e-tests/specs/editor/various/keyboard-navigable-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/keyboard-navigable-blocks.test.js
@@ -5,20 +5,18 @@ import {
 	createNewPost,
 	insertBlock,
 	pressKeyWithModifier,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
 
 const navigateToContentEditorTop = async () => {
 	// Use 'Ctrl+`' to return to the top of the editor
 	await pressKeyWithModifier( 'ctrl', '`' );
 	await pressKeyWithModifier( 'ctrl', '`' );
-
-	// Tab into the Title block
-	await page.keyboard.press( 'Tab' );
 };
 
 const tabThroughParagraphBlock = async ( paragraphText ) => {
-	// Tab to the next paragraph block
-	await page.keyboard.press( 'Tab' );
+	// Tab through the block toolbar.
+	await pressKeyTimes( 'Tab', 8 );
 
 	// The block external focusable wrapper has focus
 	const isFocusedParagraphBlock = await page.evaluate(
@@ -47,6 +45,13 @@ const tabThroughParagraphBlock = async ( paragraphText ) => {
 		() => document.activeElement.innerHTML
 	);
 	await expect( paragraphEditableContent ).toBe( paragraphText );
+
+	// Tab causes 'Open publish panel' button to receive focus.
+	await page.keyboard.press( 'Tab' );
+	const isFocusedPublishPanelButton = await page.evaluate( () =>
+		document.activeElement.classList.contains( 'edit-post-toggle-publish-panel__button' )
+	);
+	await expect( isFocusedPublishPanelButton ).toBe( true );
 };
 
 const tabThroughBlockMoverControl = async () => {
@@ -77,21 +82,20 @@ describe( 'Order of block keyboard navigation', () => {
 		for ( const paragraphBlock of paragraphBlocks ) {
 			await insertBlock( 'Paragraph' );
 			await page.keyboard.type( paragraphBlock );
-			await page.keyboard.press( 'Enter' );
 		}
+
+		// Select the middle block.
+		await page.keyboard.press( 'ArrowUp' );
+		// Move the mouse to show the block toolbar
+		await page.mouse.move( 0, 0 );
+		await page.mouse.move( 10, 10 );
 
 		await navigateToContentEditorTop();
-
-		for ( const paragraphBlock of paragraphBlocks ) {
-			await tabThroughParagraphBlock( paragraphBlock );
-		}
+		await tabThroughParagraphBlock( 'Paragraph 1' );
 
 		// Repeat the same steps to ensure that there is no change introduced in how the focus is handled.
 		// This prevents the previous regression explained in: https://github.com/WordPress/gutenberg/issues/11773.
 		await navigateToContentEditorTop();
-
-		for ( const paragraphBlock of paragraphBlocks ) {
-			await tabThroughParagraphBlock( paragraphBlock );
-		}
+		await tabThroughParagraphBlock( 'Paragraph 1' );
 	} );
 } );
